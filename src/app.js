d1f33f763585b8f1969fc900294f92666f9e34a1
Mike Jessup <jessupmr@gmail.com>
2020-12-25 15:21:02 -0700

    WIP: periodic progress push.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import confugureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = confugureStore();
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 12900, createdAt: 21000 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 4200, createdAt: 21500}));
+store.dispatch(setTextFilter('gas'));
+
+ReactDOM.render(<AppRouter />, document.getElementById('app')); 
