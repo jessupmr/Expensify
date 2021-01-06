3934baebce32086994ca822cca5a372f3f519fa3
Mike Jessup <jessupmr@gmail.com>
2021-01-06 16:57:20 -0700

    WIP: periodic checkin while adding features.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 12900 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 4200, createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Mortgage', amount: 164000 }));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+const jsx = (
+    <Provider store={ store }>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
